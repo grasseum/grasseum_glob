a2a0fa4b4c9978359cad2acb3214830aec558dac
mlph-pyap <paul.yap@monstar-lab.com>
2021-04-19 16:37:01 +0800

    Minor update in code version

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-var module_src_index =require("./util/readfilesindir")
-var validation = require("./util/validation");
-var compt = require("compts");
-var fs = require('fs');
-var path = require('path');
-module.exports = function(data,func){
-    if(compt._.has(data)){
-        data.forEach(function(v){
-            module_src_index(v,func)
-        });
-    }
-   
+const module_src_index =require("./util/readfilesindir");
+const structkit = require("structkit");
 
-}
+module.exports = function (data, func) {
+
+    if (structkit.has(data)) {
+
+        data.forEach(function (value) {
+
+            module_src_index(value, func);
+
+        });
+
+    }
+
+
+};
--- util/readfilesindir.js
+++ util/readfilesindir.js
@@ -1,180 +1,287 @@
 const fs = require("fs");
-const path = require("path");
-var compt = require("compts");
-var validation =require("grasseum_glob/util/validation")
-function srcDir(path_var,func){
-   
 
-    if(process.platform == "win32"){
-        this.reg_split = "\\"
-       path_var = path_var.replace(this.reg_split,"/")
-    }else{
-        this.reg_split = "/"
+const path = require("path");
+const structkit = require("structkit");
+const grasseumCore = require("grasseum_console");
+
+const {unixpath} = require('pathassist');
+
+const ValidRegexpFolderLookup = /(\*|!)/g;
+/**
+ * Glob reading
+ *
+ * @param {number|Object|string} pathVar The first number in an addition.
+ * @param {number|Object|string} func The second number in an addition.
+ * @returns {null} Returns the total.
+ * @example
+ *
+ * new SrcDir(pathVar,func)
+ *
+ */
+function SrcDir (pathVar, func) {
+
+
+    if (process.platform === "win32") {
+
+        this.reg_split = "\\";
+        pathVar = pathVar.replace(this.reg_split, "/");
+
+    } else {
+
+        this.reg_split = "/";
+
     }
-    this.loc_path =validation.cleanDirPath(path_var);
-    this.loc_path_split =validation.cleanDirPath(path_var).split("/");
-  
-  
+    this.loc_path =unixpath.cleanDirPath(pathVar);
+    this.loc_path_split =unixpath.cleanDirPath(pathVar).split("/");
+
     this.func = func;
 
-    this.srcDirInit()
+    this.scanDirListValue =[];
+    this.srcDirInit();
 
-   // this.srcDirFile(path_var,this.func)
 }
+SrcDir.prototype.scanDirList =  function (inc,complete_func) {
 
-srcDir.prototype.srcDirInit = function(){  
-        if( this.loc_path_split.length > 0 ){
-            if(/(\*|\!)/g.test( this.loc_path )){
+    const that = this;
+    if(this.loc_path_split.length > inc){
+        const folderIndexName =   this.loc_path_split[inc];  
+
+        if ((ValidRegexpFolderLookup).test(folderIndexName)) {
+
+            const folderIndexNameList = structkit.toArray( structkit.getValue (structkit.limit(this.loc_path_split ,0,inc -1 ) )  );
           
-                var let_data = [];
-                var main = this;
-                this.loc_path_split.forEach(function(v,k){
-                   
-
-                    if(/\*/g.test( v ) == false){
-
-                        let_data.push(v)
+            fs.readdir(folderIndexNameList.join("/") ,(error,stats) =>{  
+               if(structkit.has(error) == false){
+                structkit.each(stats,function(key,val){
+                    const valArrayFolder = structkit.arrayConcat( structkit.clone( folderIndexNameList) ,val);
+                    if( unixpath.regexpMatch(folderIndexName, val) ){
+                        if(structkit.has( that.scanDirListValue ,key)){
+                            that.scanDirListValue[key] =  valArrayFolder;
+                        }else{
+                             
+                            that.scanDirListValue.push(valArrayFolder);
+                        }
                     }
-                })
-             
-                if(let_data.length ==0){
-                    this.srcDirFile( "." )
-                }else{
-                    //this.srcDirFile( let_data.join(this.reg_split) )
-                    this.srcDirFile( let_data.join("/") )
-                }
-               // this.srcDirFile( let_data.join(this.reg_split) )
+                    
+                });
+               }
                 
+                this.scanDirList(inc+1,complete_func);
+            }); 
+           
+
+        }else{
+           
+            if(this.scanDirListValue.length ==0 ){
+                this.scanDirListValue.push([folderIndexName]);
             }else{
-                //ary_join.push(loc_path[0]);
-                //loc_path.shift();
-                this.srcDirFile(this.loc_path)
+                this.scanDirListValue.forEach((value,key) =>{
+                    that.scanDirListValue[key] =  structkit.arrayConcat(value,folderIndexName);
+                });
             }
+            this.scanDirList(inc+1,complete_func);
         }
-       // 
-      
+    }else{
+        complete_func();
+    }
     
 }
+SrcDir.prototype.srcDirInit =   async function () {
 
+    if (this.loc_path_split.length > 0) {
+        const that = this;
+        if ((ValidRegexpFolderLookup).test(this.loc_path)) {
+               
 
-
-
-srcDir.prototype.cleanFilePath = function( paths ){  
-
-    var str_path = validation.cleanDirPath(paths);
-    var split_str_path = str_path.split("/")
-    if( split_str_path.length >0 ){
-        str_path = split_str_path.slice(0, split_str_path.length-1).join("/")
-        return str_path
-    }else
-    return str_path
-
-}      
-srcDir.prototype.validateDir = function(paths){    
-    var boll= false;
-
-    var dir_split =validation.cleanDirPath(paths).split("/");
-    if (this.loc_path_split.length >= dir_split.length){
-        boll = true;
-    }
- 
-    if (boll){
-        var str_path = validation.cleanDirPath(paths).split("/");
-        var main = this;
-        this.loc_path_split.forEach(function(v,k){
-
-
-              if(dir_split.length > k){
-                  if(validation.regexpMatch( v,str_path[k] )  == false){
-                    boll = false;
-                  }
-              }
-              
-        })
-
-
-    }
-    return boll;
-}
-
-srcDir.prototype.validateFile = function(paths){    
-    var boll= false;
-
-    var dir_split =paths.split("/");
-
-    if (this.loc_path_split.length >= dir_split.length){
-        boll = true;
-    }
-
-    if (boll){
-        var str_path = validation.cleanDirPath(paths).split("/");
-        var main = this;
-        this.loc_path_split.forEach(function(v,k){
-
-          if(validation.regexpMatch( v,str_path[k] ) == false){
-            boll = false;
-          }
-           
-        })
-
-
-    }
-
-    return boll;
-}
-srcDir.prototype.srcDirFile = function(paths){    
-     
-     var main = this;
-    fs.lstat(paths, function(err, stats){
-                
-                
-        if (err == null){
-            
-        
-        if(stats.isFile()){
-           
-          let raw_basename = validation.cleanPathname(path.basename(paths))
-          if(main.validateFile( paths ) && compt._.indexOf(["grassfile.js","grasshttp.js"],raw_basename ) ==-1){
-          
-            main.func({"path":validation.cleanPathname(paths),
-                        "type":"file",
-                        "stat":stats,
-                        "filename": raw_basename,
-                        "directory":validation.cleanPathname(path.dirname(paths))
-                        });
+                this.scanDirList(0,function(){
                    
-          }
-            
-        }
-        else if(stats.isDirectory()){ 
-            
+                   that.scanDirListValue.forEach( (value) =>{
+                    that.srcDirFile(value.join("/"));
+                   });
+                });
+        } else {
 
-            fs.readdir(paths,function(errs,files){
-                if(errs==null){
-                    //if (paths =="."){
-                    //    paths = process.cwd();
-                    //}
-                    files.forEach(function(sin_file) {
-                       
-                        var local_path = path.join(paths,sin_file);
-                        if(main.validateDir( local_path ))
-                        main.srcDirFile( local_path )
-                    
-                    })
-                }else{
-                    console.log("error",errs)
+            /*
+             * Ary_join.push(loc_path[0]);
+             * loc_path.shift();
+             */
+            this.srcDirFile(this.loc_path);
+
+        }
+
+    }
+    //
+
+
+};
+
+
+SrcDir.prototype.cleanFilePath = function (paths) {
+
+    let str_path = unixpath.cleanDirPath(paths);
+    const split_str_path = str_path.split("/");
+
+    if (split_str_path.length >0) {
+
+        str_path = split_str_path.slice(0, split_str_path.length-1).join("/");
+
+        return str_path;
+
+    }
+
+    return str_path;
+
+};
+SrcDir.prototype.validateDir = function (paths) {
+
+    let boll= false;
+
+    const dirSplit =unixpath.cleanDirPath(paths).split("/");
+
+    if (this.loc_path_split.length >= dirSplit.length) {
+
+        boll = true;
+
+    }
+
+    if (boll) {
+
+        const str_path = unixpath.cleanDirPath(paths).split("/");
+
+        this.loc_path_split.forEach(function (value, key) {
+
+
+            if (dirSplit.length > key) {
+
+                if (unixpath.regexpMatch(value, str_path[key]) === false) {
+
+                    boll = false;
+
                 }
-            })
-        }
-        else{
-            console.log("Invalid filesystem")
-        }
-        }else{
-            console.log("error",err,":",this.path_var)
-        }
-    })
-}
 
-module.exports = function( loc,func){
-    var n =new srcDir(loc,func)
-}
+            }
+
+        });
+
+
+    }
+
+    return boll;
+
+};
+
+SrcDir.prototype.validateFile = function (paths) {
+
+    let boll= false;
+
+    const dirSplit =paths.split("/");
+
+    if (this.loc_path_split.length >= dirSplit.length) {
+
+        boll = true;
+
+    }
+
+    if (boll) {
+
+        const str_path = unixpath.cleanDirPath(paths).split("/");
+
+        this.loc_path_split.forEach(function (value, key) {
+
+            if (unixpath.regexpMatch(value, str_path[key]) === false) {
+
+                boll = false;
+
+            }
+
+        });
+
+
+    }
+
+    return boll;
+
+};
+SrcDir.prototype.srcDirFile = function (paths) {
+
+    const that = this;
+
+    fs.lstat(paths, function (err, stats) {
+
+
+        if (err === null) {
+
+
+            if (stats.isFile()) {
+
+                const raw_basename = unixpath.cleanPathname(path.basename(paths));
+
+                if (that.validateFile(paths) && structkit.indexOf([
+                    "grassfile.js",
+                    "grasshttp.js"
+                ], raw_basename) ===-1) {
+
+                    that.func({"directory": unixpath.cleanPathname(path.dirname(paths)),
+                        "filename": raw_basename,
+                        "path": unixpath.cleanPathname(paths),
+                        "stat": stats,
+                        "type": "file"});
+
+                }
+
+            } else if (stats.isDirectory()) {
+
+
+                fs.readdir(paths, function (errs, files) {
+
+                    if (errs===null) {
+
+                        /*
+                         * If (paths =="."){
+                         *     paths = process.cwd();
+                         * }
+                         */
+                        files.forEach(function (sin_file) {
+
+                            const local_path = path.join(paths, sin_file);
+
+                            if (that.validateDir(local_path)) {
+
+                                that.srcDirFile(local_path);
+
+                            }
+
+                        });
+
+                    } else {
+
+                        console.log("error", errs);
+
+                    }
+
+                });
+
+            } else {
+
+                grasseumCore.logRed("Invalid filesystem");
+
+            }
+
+        } else {
+
+            grasseumCore.logRed("error", err.toString(), ":", this.path_var);
+
+        }
+
+    });
+
+};
+
+module.exports = function (loc, func) {
+
+    const src =new SrcDir(loc, func);
+
+
+    return src;
+
+};
